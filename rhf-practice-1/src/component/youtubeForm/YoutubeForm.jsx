a46ca5371ff98c0959467e48d042f004c40dd9d1
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-17 11:56:25 +0530

    form state managedment of the form and validation is done through the react-hook-form library

--- rhf-practice-1/src/component/youtubeForm/YoutubeForm.jsx
+++ rhf-practice-1/src/component/youtubeForm/YoutubeForm.jsx
@@ -1,23 +1,53 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 function YoutubeForm() {
   const form = useForm();
-  const { register } = form;
+  const { register, control, handleSubmit } = form;
+  function onSubmit(data) {
+    console.log("form submitted", data);
+  }
   return (
     <div className="youtubeForm-container">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "username is required",
+            },
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+              message: "invalid email adress",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel Name</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: "channel is required",
+          })}
+        />
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 }
