349e85715b2db3ee152096c4da2ba714a75ffd01
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-22 13:01:26 +0530

    yup and @hook/resolver is installed in the preojet

--- rhf-practice-1/src/component/form/ReactHookForm.jsx
+++ rhf-practice-1/src/component/form/ReactHookForm.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 function YoutubeForm() {
-  const form = useForm();
-  const { register, handleSubmit, formState } = form;
+  const { register, handleSubmit, formState } = useForm();
   const { errors } = formState;
+
+
   function onSubmit(data) {
     console.log("form submitted", data);
   }
+
+  
   return (
     <div className="Form-container">
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
@@ -54,6 +57,51 @@ function YoutubeForm() {
         <button>Submit</button>
       </form>
     </div>
+    // <div className="Form-container">
+    //   <form onSubmit={handleSubmit(onSubmit)} noValidate>
+    //     <label htmlFor="username">Username</label>
+    //     <input
+    //       type="text"
+    //       id="username"
+    //       {...register("username", {
+    //         required: {
+    //           value: true,
+    //           message: "username is required",
+    //         },
+    //       })}
+    //     />
+    //     <p className="error">{errors.username?.message}</p>
+
+    //     <label htmlFor="email">Email</label>
+    //     <input
+    //       type="email"
+    //       id="email"
+    //       {...register("email", {
+    //         required: "email is required",
+    //         pattern: {
+    //           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i,
+    //           message: "invalid email adress",
+    //         },
+    //         validate: {
+    //           NotAdmin: (fieldValue) => {
+    //             return (
+    //               fieldValue !== "admin@example.com" ||
+    //               "Enter Different Email Address"
+    //             );
+    //           },
+    //           NotBlackListed: (fieldValue) => {
+    //             return (
+    //               !fieldValue.endsWith("baddomain.com") ||
+    //               "This domain is not supported"
+    //             );
+    //           },
+    //         },
+    //       })}
+    //     />
+    //     <p className="error">{errors.email?.message}</p>
+    //     <button>Submit</button>
+    //   </form>
+    // </div>
   );
 }
 
