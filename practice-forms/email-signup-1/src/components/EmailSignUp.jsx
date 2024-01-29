2f558ab62ef405412d5a97efa4545126698bfb01
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-29 18:31:34 +0530

    almost done with the validation with yup and yupResolver and i think the forms need some more styling

--- practice-forms/email-signup-1/src/components/EmailSignUp.jsx
+++ practice-forms/email-signup-1/src/components/EmailSignUp.jsx
@@ -3,11 +3,13 @@ import classes from "./EmailSignUp.module.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schema } from "./validation";
+import { DevTool } from "@hookform/devtools";
 
 const EmailSignUp = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
@@ -19,108 +21,115 @@ const EmailSignUp = () => {
   };
 
   return (
-    <div className={classes.formContainer}>
-      <div className={classes.header}>
-        <h1>Email Sign Up Form</h1>
-        <p>
-          We would love to be in touch with you! Please sign up to receive
-          emails from us!
-        </p>
-      </div>
-      <p>Please complete all information below:</p>
+    <>
+      <div className={classes.formContainer}>
+        <div className={classes.header}>
+          <h1>Email Sign Up Form</h1>
+          <p>
+            We would love to be in touch with you! Please sign up to receive
+            emails from us!
+          </p>
+        </div>
+        <p>Please complete all information below:</p>
 
-      <form onSubmit={handleSubmit(handleFormSubmission)} noValidate>
-        <div className={classes.nameContainer}>
-          <p>Name</p>
-          <div className={classes.nameInputContainer}>
-            <div className={classes.nameInput}>
-              <input
-                type="text"
-                className={classes.input}
-                {...register("firstName")}
-              />
-              <label htmlFor="firstName">First Name</label>
-            </div>
-            <div className={classes.nameInput}>
-              <input
-                type="text"
-                className={classes.input}
-                {...register("lastName")}
-              />
-              <label htmlFor="lastName">Last Name</label>
+        <form onSubmit={handleSubmit(handleFormSubmission)} noValidate>
+          <div className={classes.nameContainer}>
+            <p>Name</p>
+            <div className={classes.nameInputContainer}>
+              <div className={classes.nameInput}>
+                <input
+                  type="text"
+                  className={classes.input}
+                  {...register("firstName")}
+                />
+                <label htmlFor="firstName">First Name</label>
+              </div>
+              <div className={classes.nameInput}>
+                <input
+                  type="text"
+                  className={classes.input}
+                  {...register("lastName")}
+                />
+                <label htmlFor="lastName">Last Name</label>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={classes.emailContainer}>
-          <label htmlFor="email">Email</label>
-          <input type="text" className={classes.input} {...register("email")} />
-        </div>
-
-        <div className={classes.phoneContainer}>
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="number"
-            className={classes.input}
-            {...register("phone")}
-          />
-        </div>
-
-        <div className={classes.addressContainer}>
-          <p>Address</p>
-          <div className={classes.addressStreet}>
+          <div className={classes.emailContainer}>
+            <label htmlFor="email">Email</label>
             <input
               type="text"
               className={classes.input}
-              {...register("street")}
+              {...register("email")}
             />
-            <label htmlFor="street">Street Address</label>
-          </div>
-          <div className={classes.addressStreet}>
-            <input
-              type="text"
-              className={classes.input}
-              {...register("street2")}
-            />
-            <label htmlFor="street2">Street Address Line 2</label>
-          </div>
-          <div className={classes.stateAddress}>
-            <div className={classes.city}>
-              <input
-                type="text"
-                className={classes.input}
-                {...register("city")}
-              />
-              <label htmlFor="city">City</label>
-            </div>
-            <div className={classes.state}>
-              <input
-                type="text"
-                className={classes.input}
-                {...register("state")}
-              />
-              <label htmlFor="state">State / Province</label>
-            </div>
           </div>
 
-          <div className={classes.zipCode}>
+          <div className={classes.phoneContainer}>
+            <label htmlFor="phone">Phone</label>
             <input
               type="number"
               className={classes.input}
-              {...register("zip")}
+              {...register("phone")}
             />
-            <label htmlFor="zip">Postal / Zip Code</label>
           </div>
 
-          <div className={classes.textArea}>
-            <label htmlFor="textarea">
-              Comments, Questions, or Suggestions
-            </label>
-            <textarea rows="10" {...register("comments")}></textarea>
+          <div className={classes.addressContainer}>
+            <p>Address</p>
+            <div className={classes.addressStreet}>
+              <input
+                type="text"
+                className={classes.input}
+                {...register("street")}
+              />
+              <label htmlFor="street">Street Address</label>
+            </div>
+            <div className={classes.addressStreet}>
+              <input
+                type="text"
+                className={classes.input}
+                {...register("street2")}
+              />
+              <label htmlFor="street2">Street Address Line 2</label>
+            </div>
+            <div className={classes.stateAddress}>
+              <div className={classes.city}>
+                <input
+                  type="text"
+                  className={classes.input}
+                  {...register("city")}
+                />
+                <label htmlFor="city">City</label>
+              </div>
+              <div className={classes.state}>
+                <input
+                  type="text"
+                  className={classes.input}
+                  {...register("state")}
+                />
+                <label htmlFor="state">State / Province</label>
+              </div>
+            </div>
+
+            <div className={classes.zipCode}>
+              <input
+                type="number"
+                className={classes.input}
+                {...register("zip")}
+              />
+              <label htmlFor="zip">Postal / Zip Code</label>
+            </div>
+
+            <div className={classes.textArea}>
+              <label htmlFor="textarea">
+                Comments, Questions, or Suggestions
+              </label>
+              <textarea rows="10" {...register("comments")}></textarea>
+            </div>
           </div>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <DevTool control={control} />
+    </>
   );
 };
 
--- practice-forms/email-signup-1/src/components/validation.js
+++ practice-forms/email-signup-1/src/components/validation.js
@@ -1,6 +1,12 @@
 import * as yup from "yup";
 
 export const schema = yup.object().shape({
-    firstName: yup.string().required(),
-    lastName: yup.string().required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  phone: yup.number().required(),
+  street: yup.string().required(),
+  city: yup.string().required(),
+  state: yup.string().required(),
+  zip: yup.number().required(),
 });
